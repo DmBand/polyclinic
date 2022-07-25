ae03cd5ef2667d46f81cad91335f5ce2de092ebb
DmBand <d.bandysik@gmail.com>
2022-07-25 23:38:56 +0300

    responsive block height

--- polyclinic_app/static/polyclinic_app/js/block.js
+++ polyclinic_app/static/polyclinic_app/js/block.js
@@ -1,23 +1,53 @@
 'use strict'
 
-const div = document.querySelector('.polyclinics');
-
 const myObs = new ResizeObserver(entries => {
     let polyclinicDiv;
-    let divList = [];
+    let titlelist = [];
+    let addressList = [];
+    let numbersLisr = [];
+
     entries.forEach(e => {
         polyclinicDiv = e.target.childNodes;
         polyclinicDiv.forEach(function (e) {
-            if (e.tagName === 'DIV') {
-                divList.push(e.clientHeight);
-            }            
+            if (e.tagName === 'DIV')  {
+                for (let el of e.childNodes) {
+                    switch (el.className) {
+                        case 'p-title':
+                            titlelist.push(el.clientHeight);
+                            break;
+                        case 'p-address':
+                            addressList.push(el.clientHeight);
+                            break;
+                        case 'numbers':
+                            numbersLisr.push(el.clientHeight);
+                            break;
+                    }
+                }
+            }
         })
         polyclinicDiv.forEach(function (e) {
-            if (e.tagName === 'DIV') {
-                e.style.minHeight = `${Math.max.apply(null, divList)}px`;
+            if (e.tagName === 'DIV')  {
+                for (let el of e.childNodes) {
+                    switch (el.className) {
+                        case 'p-title':
+                            el.style.minHeight = `${Math.max.apply(null, titlelist)}px`;
+                            break;
+                        case 'p-address':
+                            el.style.minHeight = `${Math.max.apply(null, addressList)}px`;                            
+                            break;
+                        case 'numbers':
+                            el.style.minHeight = `${Math.max.apply(null, numbersLisr)}px`;                            
+                            break;
+                    }
+                }
             }
         })
     })
 })
 
-myObs.observe(div);
+function changeHeight() {
+    const div = document.querySelector('.polyclinics');
+    myObs.observe(div);  
+}
+
+document.addEventListener('DOMContentLoaded', changeHeight);
